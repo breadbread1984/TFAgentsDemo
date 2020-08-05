b07f3c22056ca68f585ac68a7c4bcb9129c89b35
解易 <xieyi@aksofy.cn>
2020-08-05 11:56:33 +0800

    update

--- train_ppo_cartpole.py
+++ train_ppo_cartpole.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 
 import tensorflow as tf;
+from tf_agents.drivers import dynamic_episode_driver; # data collection driver
 from tf_agents.environments import tf_py_environment, suite_gym; # environment and problem
-from tf_agents.networks.actor_distribution_network import ActorDistributionNetwork; # network structure
-from tf_agents.networks.value_network import ValueNetwork; # network structure
+from tf_agents.metrics import tf_metrics; # all kinds of metrics
+from tf_agents.eval import metric_utils; # metric utilities
+from tf_agents.networks.actor_distribution_rnn_network import ActorDistributionRnnNetwork;
+from tf_agents.networks.value_rnn_network import ValueRnnNetwork; # network structure
 from tf_agents.agents.ppo import ppo_agent; # ppo agent
-from tf_agents.trajectories import time_step, trajectory; # trajectory
 from tf_agents.replay_buffers import tf_uniform_replay_buffer; # replay buffer
-from tf_agents.policies import random_tf_policy, policy_saver; # random policy
+from tf_agents.policies import policy_saver; # random policy
 from tf_agents.utils import common; # element_wise_squared_loss
 
 batch_size = 64;
@@ -20,64 +22,73 @@ def main():
   # create agent
   optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate = 1e-3);
   tf_agent = ppo_agent.PPOAgent(
-    time_step.time_step_spec(train_env.observation_spec()),
+    train_env.time_step_spec(),
     train_env.action_spec(),
     optimizer = optimizer,
-    actor_net = ActorDistributionNetwork(train_env.observation_spec(), train_env.action_spec()),
-    value_net = ValueNetwork(train_env.observation_spec()),
+    actor_net = ActorDistributionRnnNetwork(train_env.observation_spec(), train_env.action_spec()),
+    value_net = ValueRnnNetwork(train_env.observation_spec()),
     normalize_observations = False,
+    normalize_rewards = False,
     use_gae = True,
-    lambda_value = 0.95
+    num_epochs = 25,
+    train_step_counter = tf.compat.v1.train.get_or_create_global_step()
   );
   tf_agent.initialize();
-  # reply buffer
-  replay_buffer = tf_uniform_replay_buffer.TFUniformReplayBuffer(
-    data_spec = tf_agent.collect_data_spec,
+  # replay buffer
+  replay_buffer = tf.tf_uniform_replay_buffer.TFUniformReplayBuffer(
+    tf_agent.collect_data_spec,
     batch_size = train_env.batch_size,
-    max_length = 1000000);
+    max_length = 1000000
+  );
   # shape = batch x 2 x 
-  dataset = replay_buffer.as_dataset(num_parallel_calls = 3, sample_batch_size = batch_size, num_steps = 2).prefetch(3);
-  iterator = iter(dataset);
+  dataset = replay_buffer.gather_all();
   # policy saver
-  saver = policy_saver.PolicySaver(tf_agent.policy);
+  saver = policy_saver.PolicySaver(
+    tf_agent.policy, 
+    train_step = tf.compat.v1.train.get_or_create_global_step()
+  );
+  # define trajectory collector
+  episode_count = tf_metrics.NumberOfEpisodes();
+  total_steps = tf_metrics.EnvironmentSteps();
+  avg_reward = tf_metrics.AverageReturnMetric(batch_size = train_env.batch_size);
+  avg_episode_len = tf_metrics.AverageEpisodeLengthMetric(batch_size = train_env.batch_size);
+  train_driver = dynamic_episode_driver.DynamicEpisodeDriver(
+    train_env,
+    tf_agent.collect_policy, # rollout policy
+    observers = [
+      replay_buffer.add_batch,
+      episode_count,
+      total_steps,
+      avg_reward,
+      avg_episode_len
+    ], # callbacks when an episode is completely collected
+    num_episodes = 30, # how many episodes are collected in an iteration
+  );
   # training
-  for train_iter in range(20000):
-    # collect initial trajectory to avoid a cold start
-    if train_iter == 0:
-      random_policy = random_tf_policy.RandomTFPolicy(train_env.time_step_spec(), train_env.action_spec());
-      for _ in range(1000):
-        status = train_env.current_time_step();
-        action = random_policy.action(status);
-        next_status = train_env.step(action.action);
-        traj = trajectory.from_transition(status, action, next_status);
-        replay_buffer.add_batch(traj);
-      # collect trajectory for some step every training iteration
-      for _ in range(1):
-        status = train_env.current_time_step();
-        action = tf_agent.collect_policy.action(status);
-        next_status = train_env.step(action.action);
-        traj = trajectory.from_transition(status, action, next_status);
-        replay_buffer.add_batch(traj);
-      # get a batch of dataset
-      experience, unused_info = next(iterator);
-      train_loss = tf_agent.train(experience);
-      if tf_agent.train_step_counter.numpy() % 200 == 0:
-        print('step = {0}: loss = {1}'.format(tf_agent.train_step_counter.numpy(), train_loss.loss));
-      if tf_agent.train_step_counter.numpy() % 1000 == 0:
-        # save policy
-        saver.save('checkpoints/policy_%d' % tf_agent.train_step_counter.numpy());
-        # get the average return for the updated policy
-        total_return = 0.0;
-        for _ in range(10):
-          status = eval_env.reset();
-          episode_return = 0.0;
-          while not status.is_last():
-            action = tf_agent.policy.action(status);
-            status = eval_env.step(action.action);
-            episode_return += status.reward;
-          total_return += episode_return;
-        avg_return = total_return / 10;
-        print('step = {0}: Average Return = {1}'.format(tf_agent.train_step_counter.numpy(), avg_return));
+  while total_steps.result() < 25000000:
+    global_step = tf.compat.v1.train.get_or_create_global_step();
+    if global_step % 500 == 0:
+      avg_reward.reset();
+      avg_episode_len.reset();
+      # evaluate current policy
+      eval_driver = dynamic_episode_driver.DynamicEpisodeDriver(
+        eval_env,
+        tf_agent.policy,
+        observers = [
+          avg_reward,
+          avg_episode_len,
+        ],
+        num_episodes = 30, # how many epsiodes are collected in an iteration
+      );
+      eval_driver.run();
+      print('step = {0}: Average Return = {1} Average Episode Length = {2}'.format(tf.compat.v1.train.get_or_create_global_step(), avg_reward.result(), avg_episode_len.result()));
+    train_driver.run();
+    trajectories = replay_buffer.gather_all();
+    loss, _ = tf_agent.train(experience = trajectories);
+    replay_buffer.clear(); # clear collected episodes right after training
+    if tf.compat.v1.train.get_or_create_global_step() % 500 == 0:
+      saver.save('checkpoints/policy_%d' % tf_agent.train_step_counter.numpy());
+  # TODO
   # play cartpole for the last 3 times and visualize
   import cv2;
   for _ in range(3):
